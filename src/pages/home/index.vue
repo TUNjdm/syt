7e5705b375ea0c0e0868fd3e23a5d9f1e2f7b4a2
hgz <1243048280@qq.com>
2025-09-18 02:43:44 +0800

    card组件数据渲染

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -14,7 +14,7 @@
         <!-- 展示医院卡片 -->
         <div class="card-box">
           <!-- 循环插入多张医院卡片 -->
-          <card class="card-item" v-for="index in 9" :key="index" />
+          <card class="card-item" v-for="item in hospitalArr" :key="item.id" :hospitalInfo="item" />
         </div>
           <!-- 分页器 -->
           <div class="page-box">
@@ -34,6 +34,10 @@
 </template>
 
 <script setup lang="ts">
+// 引入组合式api函数
+import { onMounted, ref } from 'vue';
+import { reqHospital } from '@/api/home/index';
+
 // 引入子组件
 import carousel from "../home/carousel/index.vue";
 import Search from "../home/search/index.vue";
@@ -41,8 +45,12 @@ import level from "../home/level/index.vue";
 import region from "../home/region/index.vue";
 import card from "../home/card/index.vue";
 
-// 引入分页器组件需要的数据
-import { ref } from 'vue'
+interface Hospital {
+  id: string;
+  // 可以根据实际返回数据添加更多字段
+  [key: string]: any;
+}
+
 // 分页器页码
 const pageNo = ref<number>(1)
 // 一页展示几条数据
@@ -50,11 +58,25 @@ const pageSize = ref<number>(10)
 const size = ref('small')
 const disabled = ref(false)
 const background = ref(true)
-const handleSizeChange = (val: number) => {
-  pageSize4.value = val
-}
-const handleCurrentChange = (val: number) => {
-  currentPage4.value = val
+// 存储已有医院数据
+const hospitalArr = ref<Hospital[]>([])
+// 存储医院总个数
+const total = ref<number>(0)
+// 组件挂载完毕发请求
+onMounted(() => {
+  getHospitalInfo()
+})
+// 获取已有医院数据
+const getHospitalInfo = async() => { 
+  // 获取医院数据
+  await reqHospital(pageNo.value, pageSize.value).then(res => {
+    console.log('获取到的医院数据:', res);
+    // 存储已有医院数据
+    hospitalArr.value = res.data.content
+    // 存储医院总个数
+    total.value = res.data.totalElements
+    console.log('数组值:', hospitalArr.value);
+})
 }
 </script>
 <style lang="scss">
